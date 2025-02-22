c4c415944119f59d9151229ee9ffa07b13bb62d1
Lokesh Kumar <lokesh2.yss@gmail.com>
2025-02-23 02:39:54 +0530

    Added carousel to the top just below the header section

--- src/app/hotel-details/index.jsx
+++ src/app/hotel-details/index.jsx
@@ -4,21 +4,27 @@ import HotelMetaDetails from './hotel-meta-details'
 import HotelRoomsPicker from './hotel-rooms-picker'
 import HotelPolicy from './hotel-policy'
 import HotelCheckoutCard from './hotel-checkout-card'
+import { HOTEL_DATA, HOTEL_INFO } from './hotel-details-dummy-data'
 
 const HotelDetails = () => {
-  return (
-    <div className='container'>
-        <PropertyViewCarousel />
-        <div>
-            <div>
-                <HotelMetaDetails />
-                <HotelRoomsPicker />
-                <HotelPolicy />
+    const hotelData = HOTEL_DATA;
+    const hotelInfo = HOTEL_INFO;
+    return (
+        <div className='container mt-6 mb-12'>
+            <PropertyViewCarousel images ={hotelData.hotel.photos}/>
+            <div className='flex gap-6 mt-6'>
+                <div className='flex-1 space-y-8'>
+                    <HotelMetaDetails hotel={hotelData}/>
+                    <HotelRoomsPicker />
+                    <HotelPolicy />
+                </div>
+                <aside className='w-[340px] shrink-0 border border-border shadow-md rounded-xl'>
+                    <HotelCheckoutCard />
+                </aside>
+                
             </div>
-            <HotelCheckoutCard />
         </div>
-    </div>
-  )
+    )
 }
 
 export default HotelDetails
--- src/app/hotel-details/property-view-carousel.jsx
+++ src/app/hotel-details/property-view-carousel.jsx
@@ -1,8 +1,27 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import React from 'react'
 
-const PropertyViewCarousel = () => {
+const PropertyViewCarousel = ({images}) => {
   return (
-    <div>PropertyViewCarousel</div>
+    <section>
+        <Carousel 
+            opts= {{
+                breakpoints: {
+                    '(min-width:1024px)': {slidesToScroll:2},
+                },
+            }}
+            className="overflow-hidden rounded-lg">
+            <CarouselContent className="-ml-0.5">
+                {images.map((image, index) => (
+                    <CarouselItem key={index} className="lg:basis-1/2 pl-0.5">
+                        <img className='h-96 w-full object-cover' src={image} alt={`Image index: ${index}`} />
+                    </CarouselItem>
+                ))};
+            </CarouselContent>
+            <CarouselPrevious className="left-1 shadow-lg"/>
+            <CarouselNext className="right-1 shadow-lg" />
+        </Carousel>
+    </section>
   )
 }
 
