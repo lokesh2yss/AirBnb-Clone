8c4e46b6fcc3df1dbdef9663b13e5f7f5e19503e
Lokesh Kumar <lokesh2.yss@gmail.com>
2025-03-19 13:26:24 +0530

    Added search form on the home page

---
+++ src/app/auth/index.jsx
@@ -0,0 +1,23 @@
+import AuthLayout from '@/components/layouts/auth.layout'
+import React from 'react'
+import SignIn from './signin'
+import SignUp from './signup'
+
+const SignInPage = () => {
+  return (
+    <AuthLayout title="Welcome Back" description="Please enter your details to Sign in">
+        <SignIn />
+    </AuthLayout>
+  )
+}
+
+const SignUpPage = () => {
+    return (
+      <AuthLayout title="Create Account" description="Please fill the form to Sign up ">
+          <SignUp />
+      </AuthLayout>
+    )
+  }
+  
+
+export { SignInPage, SignUpPage }
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,12 +3,17 @@ import Home from './home'
 import Header from '@/components/layouts/header.layout'
 import Footer from '@/components/layouts/footer.layout'
 import HotelDetails from './hotel-details'
+import { SignInPage } from './auth'
+import { SignUpPage } from './auth'
 const App = () => {
   return (
     <div>
         <Header/>
-        {/* <Home/> */}
-        <HotelDetails />
+        <Home/>
+        {/* <HotelDetails /> */}
+        {/* <AuthPage /> */}
+        {/* <SignInPage /> */}
+        {/* <SignUpPage /> */}
         <Footer/>
     </div>
   )
